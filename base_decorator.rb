1d5b800c8866376a72652d8835cdaf790dc51b14
Ritika Rawat <Ritikarawat220@gmail.com>
2023-05-30 18:03:50 +0530

    Update changes

--- base_decorator.rb
+++ base_decorator.rb
@@ -1,7 +1,6 @@
 require_relative 'nameable'
 class BaseDecorator < Nameable
   def initialize(nameable)
-    super
     @nameable = nameable
   end
 
--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -1,4 +1,4 @@
-require_relative 'baseDecorator'
+require_relative 'base_decorator'
 class CapitalizeDecorator < BaseDecorator
   def correct_name
     @nameable.correct_name.capitalize
