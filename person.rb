3fd788cee128141595784121b8ddfc28ac8bbfd8
Ritika Rawat <Ritikarawat220@gmail.com>
2023-05-30 17:05:16 +0530

    Add Person class to Nameable

--- person.rb
+++ person.rb
@@ -1,7 +1,9 @@
-class Person
+require_relative 'nameable'
+class Person < Nameable
   attr_accessor :id, :name, :age
 
   def initialize(age, parent_permission: true, name: 'Unknown')
+    super
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -18,5 +20,9 @@ class Person
 
   def can_use_services?
     of_age? || parent_permission
+  end
+
+  def correct_name
+    @name
   end
 end
