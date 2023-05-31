d2592efeabd5b090265f0edb4a7968ae939ebcac
Ritika Rawat <Ritikarawat220@gmail.com>
2023-05-31 19:29:06 +0530

    Update classroom.rb file

--- classroom.rb
+++ classroom.rb
@@ -13,3 +13,15 @@ class Classroom
     student.classroom = self
   end
 end
+
+classroom = Classroom.new('Physics')
+student1 = Student.new(10, nil, name: 'Eva')
+student2 = Student.new(12, nil, name: 'Frank')
+student3 = Student.new(11, nil, name: 'Grace')
+
+classroom.add_student(student1)
+classroom.add_student(student2)
+classroom.add_student(student3)
+
+puts classroom.students.map(&:name)
+
