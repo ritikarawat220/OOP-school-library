982749471e3510c338d4300ab734e46f7854f966
Ritika Rawat <Ritikarawat220@gmail.com>
2023-06-01 16:05:04 +0530

    Update rental.rb

--- rental.rb
+++ rental.rb
@@ -14,20 +14,3 @@ class Rental
     person.add_rental(self)
   end
 end
-
-book3 = Book.new('Harry Potter and the Sorcerer\'s Stone', 'J.K. Rowling')
-book4 = Book.new('Pride and Prejudice', 'Jane Austen')
-
-person3 = Person.new(30, name: 'Emma')
-person4 = Person.new(19, name: 'Daniel', parent_permission: false)
-
-rental3 = Rental.new(Date.today, book3, person3)
-rental4 = Rental.new(Date.today, book4, person4)
-
-p rental3.date
-p rental3.book.title
-p rental3.person.name
-
-p rental4.date
-p rental4.book.title
-p rental4.person.name
